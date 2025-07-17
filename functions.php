3c49b6f7beab8aa6f3be7054fbda6433dcdebb42
ida-work <i21155388@gmail.com>
2025-07-18 06:55:02 +0900

    first commit

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function enqueue_parent_theme_styles()
+{
+    wp_enqueue_style(
+        'parent-style',
+        get_template_directory_uri() . '/style.css',
+        array(),
+        filemtime(get_template_directory() . '/style.css')
+    );
+}
+add_action('wp_enqueue_scripts', 'enqueue_parent_theme_styles');
